70e4e90a9c28a6d62b5dc5b6a35f2921618d8ae9
Sammy Hajhamid <pokeso123@hotmail.com>
2020-02-24 21:19:23 -0800

    finished bot

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client} = require('discord.js');
+const tokens = Array.from(require("./accounts").tokens);
 
+let selectedBot = 0;
+//                     accounts[1].channels.get("681189296702881933").send(++currentCount)
+
+const accounts = tokens.map(token => {
+    let client = new Client();
+    client.login(token);
+    return client;
+});
+
+accounts[0].on("message", msg => {
+    if (msg.content.match(/^\d+$/) && msg.channel.id === "681189296702881933") {
+        accounts[selectedBot].channels.get("681189296702881933").send((parseInt(msg.content) + 1).toString());
+        if (selectedBot + 1 === accounts.length) {
+            selectedBot = 0;
+        } else {
+            ++selectedBot;
+        }
+    }
+});
